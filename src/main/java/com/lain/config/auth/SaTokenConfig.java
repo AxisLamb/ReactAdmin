30d272b5f2f78d68cbe29b4614dabc04041c47d6
Carter Liu <Carter.gy.liu@pccw.com>
2026-08-17 17:11:47 +0800

    init

--- src/main/java/com/lain/config/auth/SaTokenConfig.java
+++ src/main/java/com/lain/config/auth/SaTokenConfig.java
@@ -2,22 +2,17 @@ package com.lain.config.auth;
 
 import cn.dev33.satoken.interceptor.SaInterceptor;
 import cn.dev33.satoken.stp.StpUtil;
-import com.lain.config.oss.properties.LocalObjectStorageProperties;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableConfigurationProperties({
-    LocalObjectStorageProperties.class
-})
 public class SaTokenConfig implements WebMvcConfigurer {
 
-    @Value("${file.base-path}")
+    @Value("${os.file.local.base-path}")
     private String basePath;
 
     @Autowired
